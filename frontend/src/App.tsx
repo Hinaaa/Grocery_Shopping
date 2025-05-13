dfed1e8b87c8d2c19102997a7c4fa159cda6c134
hinajawaid <hinajawaid.h@gmail.com>
2025-05-13 15:34:51 +0200

    Project cleaned

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,9 @@
-import {useEffect, useState} from 'react'
 import './App.css'
 
 function App() {
-  const [message, setMessage] = useState("")
-    useEffect(() => {
-        fetch("http://localhost:8080/api/hello")
-            .then((res) => res.text())
-            .then(data=>setMessage(data))
-            .catch(error => console.error('Error fetching hello:', error));
-    }, []);
 
   return (
     <>
-        <h1>{message}</h1>
     </>
   )
 }
